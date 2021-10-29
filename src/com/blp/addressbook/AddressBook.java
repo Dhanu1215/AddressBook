5a3aa090adf77b0fcad5ce26cfc7be83753e5836
Dhanashree hakke <dhanashreehakke7@gmail.com>
2021-10-29 15:42:21 +0530

    Added: Add contacts file is added

--- src/com/blp/addressbook/AddressBook.java
+++ src/com/blp/addressbook/AddressBook.java
@@ -11,43 +11,44 @@ public class AddressBook {
     public static void main(String[] args) {
         System.out.println("Welcome to Address book program");
         Scanner sc = new Scanner(System.in);
-        System.out.println(" 0. Exit. ");
         System.out.println(" 1. Add contact. ");
         System.out.println(" 2. Edit contact. ");
         System.out.println(" 3. Delete contact. ");
         int choice = sc.nextInt();
-        switch (choice) {
-            case 1:
-                System.out.println("Enter Contact Details");
-                System.out.println("Enter firstname");
-                String firstName = sc.next();
-                System.out.println("Enter lastname");
-                String lastName = sc.next();
-                System.out.println("Enter address");
-                String address = sc.next();
-                System.out.println("Enter city");
-                String city = sc.next();
-                System.out.println("Enter state");
-                String state = sc.next();
-                System.out.println("Enter zip");
-                int zip = sc.nextInt();
-                System.out.println("Enter mobile number");
-                long mobNum = sc.nextLong();
-                System.out.println("Enter email-Id");
-                String emailId = sc.next();
-                AddressBook contact1 = new AddressBook();
-                contact1.addContact(firstName, lastName, address, city, state, zip, mobNum, emailId);
-                break;
-            case 2:
-                System.out.println("Edit Contact");
-                break;
-            case 3:
-                System.out.println("Delete Contact");
-                break;
-            default:
-                System.out.println("Enter valid Number");
+        while (choice != 0) {
+            switch (choice) {
+                case 1:
+                    System.out.println("Enter Contact Details");
+                    System.out.println("Enter firstname");
+                    String firstName = sc.next();
+                    System.out.println("Enter lastname");
+                    String lastName = sc.next();
+                    System.out.println("Enter address");
+                    String address = sc.next();
+                    System.out.println("Enter city");
+                    String city = sc.next();
+                    System.out.println("Enter state");
+                    String state = sc.next();
+                    System.out.println("Enter zip");
+                    int zip = sc.nextInt();
+                    System.out.println("Enter mobile number");
+                    long mobNum = sc.nextLong();
+                    System.out.println("Enter email-Id");
+                    String emailId = sc.next();
+                    AddressBook contact1 = new AddressBook();
+                    contact1.addContact(firstName, lastName, address, city, state, zip, mobNum, emailId);
+                    break;
+                case 2:
+                    System.out.println("Edit Contact");
+                    break;
+                case 3:
+                    System.out.println("Delete Contact");
+                    break;
+                default:
+                    System.out.println("Enter valid Number");
+            }
+            System.out.println("Program ended..Thank You");
         }
-        System.out.println("Program ended..Thank You");
     }
 
     private void addContact(String firstName, String lastName, String address, String city, String state, int zip, long mobNum, String emailId) {
