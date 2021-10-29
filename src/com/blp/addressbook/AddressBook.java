49eb6fbed0bbd9a0d5ed817732d28eb145c2dda6
Dhanashree hakke <dhanashreehakke7@gmail.com>
2021-10-29 10:33:49 +0530

    Added: Welcome to Address Book Program file is added

---
+++ src/com/blp/addressbook/AddressBook.java
@@ -0,0 +1,11 @@
+package com.blp.addressbook;
+
+/**
+ * Address book creation
+ */
+public class AddressBook {
+
+    public static void main(String[] args) {
+        System.out.println("Welcome to Address book program");
+    }
+}
